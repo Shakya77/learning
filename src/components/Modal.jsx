3aa6e9d5b0b5d2ff1c7e542d04168297dff2c740
Shakya77 <bijanshakya145@gmail.com>
2025-02-12 00:23:25 +0545

    changes done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,9 @@ export default function Modal({ children, handleClose }) {
     return ReactDOM.createPortal((
         <div className="modal-backdrop">
             <div className="modal">
-                {children}
+                <h2>
+                    {children}
+                    </h2>
                 <button onClick={handleClose}>Close</button>
             </div>
         </div>
